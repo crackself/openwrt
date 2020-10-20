cf74c8484326655efd6d4f35cbe6330f60aff9e4
crackself <crackself@gmail.com>
2020-10-20 09:07:58 +0000

    update vlmcsd

--- package/crackself/luci-app-vlmcsd/luasrc/controller/vlmcsd.lua
+++ package/crackself/luci-app-vlmcsd/luasrc/controller/vlmcsd.lua
@@ -5,7 +5,15 @@ function index()
 		return
 	end
 	local page
-	page = entry({"admin", "services", "vlmcsd"}, cbi("vlmcsd"), _("KMS激活服务"), 100)
+	page = entry({"admin", "services", "vlmcsd"}, cbi("vlmcsd"), _("KMS Server"), 100)
 	page.i18n = "vlmcsd"
 	page.dependent = true
+	entry({"admin","services","vlmcsd","status"},call("act_status")).leaf=true
+end
+
+function act_status()
+  local e={}
+  e.running=luci.sys.call("pgrep vlmcsd >/dev/null")==0
+  luci.http.prepare_content("application/json")
+  luci.http.write_json(e)
 end
