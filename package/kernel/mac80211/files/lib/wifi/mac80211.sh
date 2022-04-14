87fc76b45c14f9d7d8c4637334d47aeb7aca1519
crackself <crackself@gmail.com>
2022-04-14 13:25:50 +0000

    custom setting

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -166,6 +166,11 @@ detect_mac80211() {
 		else
 			dev_id="set wireless.radio${devidx}.macaddr=$(cat /sys/class/ieee80211/${dev}/macaddress)"
 		fi
+		if [ x$mode_band == x"a" ]; then
+			ssid_5ghz="-5G"
+		else
+			ssid_5ghz=""
+		fi
 
 		uci -q batch <<-EOF
 			set wireless.radio${devidx}=wifi-device
@@ -180,8 +185,9 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
-			set wireless.default_radio${devidx}.encryption=none
+			set wireless.default_radio${devidx}.ssid=My-Router${ssid_5ghz}
+			set wireless.default_radio${devidx}.encryption=psk2
+			set wireless.default_radio${devidx}.key=crackself
 EOF
 		uci -q commit wireless
 
